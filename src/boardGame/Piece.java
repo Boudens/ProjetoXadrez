967d4d5fbfac7011623070c0b2b6e827ceda9b7a
Caio Boudens <caioboudens00@gmail.com>
2023-09-18 18:10:06 -0300

    Implementando movimentos possiveis da torre

--- src/boardGame/Piece.java
+++ src/boardGame/Piece.java
@@ -12,18 +12,18 @@ public abstract class Piece {
 	protected Board getBoard() {
 		return board;
 	}
-	
-	public abstract  boolean [][] possibleMoves();
-	
-	public boolean possibleMove(Position positon) {
-		return possibleMoves()[positon.getRow()][position.getColumn()];
+
+	public abstract boolean[][] possibleMoves();
+
+	public boolean possibleMove(Position position) {
+		return possibleMoves()[position.getRow()][position.getColumn()];
 	}
-	
+
 	public boolean isThereAnyPossibleMove() {
 		boolean[][] mat = possibleMoves();
 		for (int i = 0; i < mat.length; i++) {
 			for (int j = 0; j < mat.length; j++) {
-				if(mat[i][j]) {
+				if (mat[i][j]) {
 					return true;
 				}
 			}
